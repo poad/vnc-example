3cf7ea9e5d9066d3ee2bd24bffeb06bba9c3c9e7
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-15 17:11:08 +0900

    Add the analyzer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <Component {...pageProps} />
