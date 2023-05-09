bd78fca366d6faa7fb28cca91fc35e1f3a1b1d20
Kenji Saito <ken-yo@mbr.nifty.com>
2023-05-10 07:38:10 +0900

    Migrate to Next.js 13.4

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+'use client';
+import { ReactNode } from 'react';
+import 'tailwindcss/tailwind.css';
+import './styles/Layout.module.css';
+
+export default function Document({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
